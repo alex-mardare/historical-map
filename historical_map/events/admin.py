bbf83ad37c783ceb593cf1a30cc7dadd05241c8a
Alexandru Mardare <alexandru.nicolae.mardare@gmail.com>
2022-10-19 21:21:41 +0200

    add events model

--- historical_map/events/admin.py
+++ historical_map/events/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import HistoricalEvent
 
 # Register your models here.
+admin.site.register(HistoricalEvent)
--- historical_map/events/models.py
+++ historical_map/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class HistoricalEvent(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    time = models.TimeField(null=True)
+    description = models.CharField(max_length=1000)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+
+    def __str__(self):
+        return self.name
