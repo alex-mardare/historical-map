97dec5856bf7962238a46f81b37d3b2f6c1365d5
Alexandru Mardare <alexandru.nicolae.mardare@gmail.com>
2022-10-25 21:23:49 +0200

    HistoricalEvent model updates
    * add two new fields
    * fix issue when time was still mandatory

--- historical_map/events/models.py
+++ historical_map/events/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class HistoricalEvent(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateField()
-    time = models.TimeField(null=True)
+    time = models.TimeField(null=True, blank=True)
     description = models.CharField(max_length=1000)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
