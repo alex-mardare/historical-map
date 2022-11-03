7587386a531a3cfae33e065a7b9bed37d21520c6
Alexandru Mardare <alexandru.nicolae.mardare@gmail.com>
2022-11-03 15:19:02 +0100

    add historical event list endpoint

--- historical_map/events/views.py
+++ historical_map/events/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from rest_framework import generics
+
+from .models import HistoricalEvent
+from .serializers import HistoricalEventSerializer
 
 # Create your views here.
 def test_home(request):
     return HttpResponse('Historical Map WIP')
+
+class HistoricalEventList(generics.ListAPIView):
+    queryset = HistoricalEvent.objects.all()
+    serializer_class = HistoricalEventSerializer
