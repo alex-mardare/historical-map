f04785ab36bdd57eb3f6c14331de3817dcf3a665
Alexandru Mardare <alexandru.nicolae.mardare@gmail.com>
2023-02-18 13:05:43 +0100

    extract events loading into home page

--- frontend_historical_map/src/components/events/EventsList.js
+++ frontend_historical_map/src/components/events/EventsList.js
@@ -1,28 +1,14 @@
 import { Table } from 'antd';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { DEV_API_EVENTS_APP_BASE_URL } from '../config/constants/endpoints';
 import { columnsConfig } from '../config/tables/EventsListColumnsConfig';
 
 
-export default function EventsList() {
-    const [events, setEvents] = useState([])
-
-    useEffect(() => {
-        axios.get(DEV_API_EVENTS_APP_BASE_URL)
-            .then(res => {
-                setEvents(res.data);
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }, []);
-
+export default function EventsList(props) {
     return(
         <Table 
             columns={columnsConfig}
-            dataSource={events}
+            dataSource={props.events}
             rowKey={(event) => event.id}
         />
     )
--- frontend_historical_map/src/components/pages/Home.js
+++ frontend_historical_map/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import { Button, Form, Modal } from 'antd';
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { DEV_API_EVENTS_APP_BASE_URL } from '../config/constants/endpoints';
 import EventCreateForm from '../events/EventCreateForm';
@@ -10,10 +10,13 @@ function Home() {
   const [form] = Form.useForm();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [events, setEvents] = useState([])
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  }
+  useEffect(() => {
+    axios.get(DEV_API_EVENTS_APP_BASE_URL)
+        .then(res => { setEvents(res.data); })
+        .catch(error => { console.log(error); })
+  }, []);
 
   const handleCancel = () => {
     setIsModalOpen(false);
@@ -24,14 +27,23 @@ function Home() {
     setIsModalOpen(false);
   }
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  }
+
   const submitForm = (values) => {
     axios.post(DEV_API_EVENTS_APP_BASE_URL, values)
+      .then(() => {
+        axios.get(DEV_API_EVENTS_APP_BASE_URL)
+          .then(res => { setEvents(res.data); })
+          .catch(error => { console.log(error); })
+      })
       .catch(error => console.log(error));
   }
 
   return (
     <div className="App">
-      <EventsList/>
+      <EventsList events={events}/>
       <Button onClick={showModal} type='primary'> Create Event </Button>
       <Modal
         okText='Submit'
