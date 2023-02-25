880ede1c592613a3d6bd1dbf5203acdb19ea7232
Alexandru Mardare <alexandru.nicolae.mardare@gmail.com>
2023-02-25 17:10:57 +0100

    fix issue with marker icon

---
+++ frontend_historical_map/src/components/elements/customIconLeaflet.js
@@ -0,0 +1,13 @@
+import L from 'leaflet';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+export const customIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
--- frontend_historical_map/src/components/pages/Home.js
+++ frontend_historical_map/src/components/pages/Home.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 import { DEV_API_EVENTS_APP_BASE_URL } from '../config/constants/endpoints';
 import { eventsLoadingError } from '../config/notifications/events';
+import { customIcon } from '../elements/customIconLeaflet';
 
 import "leaflet/dist/leaflet.css";
 
@@ -18,9 +19,9 @@ function Home() {
 
   return (
     <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={false} style={{ height: "100vh", width: "100vw" }}>
+      <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true} style={{ height: "100vh", width: "100vw" }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[51.505, -0.09]}>
+          <Marker icon={customIcon} position={[51.505, -0.09]}>
             <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
           </Marker>
         </MapContainer>
