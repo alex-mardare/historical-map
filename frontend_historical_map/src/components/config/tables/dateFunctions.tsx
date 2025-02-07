69bde57f0755453ea2c31ed3de066472986bc3d3
Alexandru Mardare <alexandru.nicolae.mardare@gmail.com>
2025-02-07 20:55:47 +0100

    update date display functionality

--- frontend_historical_map/src/components/config/tables/dateFunctions.tsx
+++ frontend_historical_map/src/components/config/tables/dateFunctions.tsx
@@ -1,51 +1,60 @@
-import { HistoricalDateObject } from "../../models/types/historicalDateObject";
+import { HistoricalDateObject } from '../../models/types/historicalDateObject'
 
+function convertDateToString(date: string | null): Date | undefined {
+  const dateArr = extractDateFromString(date)
+  if (dateArr !== undefined) {
+    const year = Number(dateArr[0])
+    const month = dateArr[1] ? Number(dateArr[1]) - 1 : 0
+    const day = dateArr[2] ? Number(dateArr[2]) : 1
 
-export function convertDateToString(date: string | null) : Date | undefined {
-    const dateArr = extractDateFromString(date);
-    if (dateArr !== undefined) {
-        const year = Number(dateArr[0])
-        const month = dateArr[1] ? Number(dateArr[1]) - 1 : 0
-        const day = dateArr[2] ? Number(dateArr[2]) : 1
-    
-        return new Date(year, month, day)
-    }
+    return new Date(year, month, day)
+  }
 }
 
-export function dateColumnSort(firstObjectDate: HistoricalDateObject, secondObjectDate: HistoricalDateObject, firstDate: Date | undefined, secondDate: Date | undefined) {
-    // For negative dates the order is reversed
-    const isFirstDateNegative = firstObjectDate.date && firstObjectDate.date[0] === '-'
-    const isSecondDateNegative = secondObjectDate.date && secondObjectDate.date[0] === '-'
+function dateColumnSort(
+  firstObjectDate: HistoricalDateObject,
+  secondObjectDate: HistoricalDateObject,
+  firstDate: Date | undefined,
+  secondDate: Date | undefined
+) {
+  // For negative dates the order is reversed
+  const isFirstDateNegative =
+    firstObjectDate.date && firstObjectDate.date[0] === '-'
+  const isSecondDateNegative =
+    secondObjectDate.date && secondObjectDate.date[0] === '-'
 
-    if (isFirstDateNegative && !isSecondDateNegative) return -1
-    if (!isFirstDateNegative && isSecondDateNegative) return 1
+  if (isFirstDateNegative && !isSecondDateNegative) return -1
+  if (!isFirstDateNegative && isSecondDateNegative) return 1
 
-    if (firstDate === undefined) return secondDate === undefined ? 0 : 1
-    if (secondDate === undefined) return -1
+  if (firstDate === undefined) return secondDate === undefined ? 0 : 1
+  if (secondDate === undefined) return -1
 
-    if (firstDate !== secondDate) return firstDate > secondDate ? 1 : -1
+  if (firstDate !== secondDate) return firstDate > secondDate ? 1 : -1
 
-    if (firstObjectDate.time === null) return secondObjectDate.time === null ? 0 : 1
-    if (secondObjectDate.time === null) return -1
+  if (firstObjectDate.time === null)
+    return secondObjectDate.time === null ? 0 : 1
+  if (secondObjectDate.time === null) return -1
 
-    if (firstObjectDate.time !== secondObjectDate.time) {
-        if (firstObjectDate.time > secondObjectDate.time) return 1
-        else return -1
-    }
-    else return 0
+  if (firstObjectDate.time !== secondObjectDate.time) {
+    if (firstObjectDate.time > secondObjectDate.time) return 1
+    else return -1
+  } else return 0
 }
 
-function extractDateFromString(date : string | null): String[] | undefined {
-    if (date) {
-        let dateComponents = []
-        if (date[0] === '-') {
-            dateComponents = date.substring(1).split('-')
-            dateComponents[0] = '-'.concat(dateComponents[0])
-        }
-        else {
-            dateComponents = date.split('-')
-        }
-    
-        return dateComponents
+function extractDateFromString(
+  date: string | null | undefined
+): String[] | undefined {
+  if (date) {
+    let dateComponents = []
+    if (date[0] === '-') {
+      dateComponents = date.substring(1).split('-')
+      dateComponents[0] = '-'.concat(dateComponents[0])
+    } else {
+      dateComponents = date.split('-')
     }
+
+    return dateComponents
+  }
 }
+
+export { convertDateToString, dateColumnSort, extractDateFromString }
--- frontend_historical_map/src/components/utils/display/displayDates.tsx
+++ frontend_historical_map/src/components/utils/display/displayDates.tsx
@@ -1,13 +1,35 @@
+import { extractDateFromString } from '../../config/tables/dateFunctions'
+
 const displayHumanReadableDate = (date: string | null | undefined) => {
-  if (date === null || date === undefined) {
+  const dateParts = extractDateFromString(date)
+
+  if (date === null || date === undefined || dateParts === undefined) {
     return 'N/A'
   }
-  return new Date(date).toLocaleDateString('en-GB', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    timeZone: 'UTC'
-  })
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+  const formatYear =
+    date[0] === '-'
+      ? `${Math.abs(Number(dateParts[0]))} BC`
+      : `${dateParts[0]} AD`
+  const formatMonth = dateParts[1] ? monthNames[Number(dateParts[1]) - 1] : ''
+  const formatDay = dateParts[2] ?? ''
+
+  return formatDay + ' ' + formatMonth + ' ' + formatYear
 }
 
 export { displayHumanReadableDate }
